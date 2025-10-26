a567e81b53b25f8d0e5b9d79743ec9f517c24035
Abhishek R.S <rsabhishek77@gmail.com>
2025-10-26 19:30:42 +0530

    feat(ui):basic frondent setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Route, Routes, Outlet } from 'react-router-dom'
+import Home from './pages/home'
+import Products from './pages/products'
+import SellerHome from './pages/sellerHome'
+import Dashboard from './pages/dashboard'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+
+  const Layout = () => (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' element={ <Home/>} />
+        <Route path='/products' element={ <Products/>} />
+
+        <Route path='/seller'>
+          <Route path='' element={<SellerHome />}/>
+          <Route path='dashboard' element={ <Dashboard />} />
+        </Route>
+      </Route>
+    </Routes>
+   
   )
 }
 
